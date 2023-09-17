b3461b819bb760d2180c4880360f20c099541115
Ashish Kumar <76085126+Ashish15678@users.noreply.github.com>
2023-09-17 15:25:39 +0000

    signup login ui complete

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,11 @@
 "use client";
 import React from "react";
 
-const submit = (e) => {
-  e.preventDefault();
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  console.log(name, email, password);
-};
+const Login = () => {
+  const submit = (e) => {
+    e.preventDefault();
+  };
 
-const signup = () => {
   return (
     <div>
       <main className="signupmain">
@@ -18,13 +14,6 @@ const signup = () => {
         </section>
         <form action="post">
           <input type="text" name="" placeholder="Enter your name" id="name" />
-          <br />
-          <input
-            type="text"
-            name=""
-            id="email"
-            placeholder="Enter your email"
-          />{" "}
           <br />
           <input
             type="password"
@@ -36,9 +25,8 @@ const signup = () => {
           <button type="button" onSubmit={submit}>
             Submit
           </button>
-          <br />
-          <a href="/login">
-            <button type="button">Login</button>
+          <a href="/signup">
+            <button type="button">Signup</button>
           </a>
         </form>
       </main>
@@ -46,4 +34,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default Login;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import Image from 'next/image'
+import react from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  )
-}
+    <>
+      <main>
+        <section className="veryTopComponent">
+          <section className="topHeading">
+            <center>
+              <h1>QuackDocs</h1>
+            </center>
+          </section>
+          <a href="/signup">
+            <button className="signUp">Signup</button>
+          </a>
+        </section>
+        <section className="subHeading">
+          <h3>Why us ?</h3>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
